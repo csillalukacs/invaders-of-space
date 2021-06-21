b12582a13da0e61baaf99e16c462f61bed4a1e5e
Tóth Dániel <td.toth.dani@gmail.com>
2021-06-21 15:12:54 +0200

    feat: create 20x20 board

--- game.js
+++ game.js
@@ -1,7 +1,18 @@
 initGame();
 
 function initGame() {
-
-    // Your game can start here, but define separate functions, don't write everything in here :)
-
+    createBoardDivs();
+}
+function createBoardDivs() {
+    const grid = document.querySelector('.grid');
+    let row = -1;
+    for (let i = 0; i < 400; i++) {
+        if (i % 20 === 0) {
+            row++;
+        }
+        grid.insertAdjacentHTML(
+            'beforeend',
+            `<div class="square" data-row="${row}" data-col="${(i%20)}"></div>`
+        );
+    }
 }
