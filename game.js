c933acb03044b6308f25af6de6a538fdc4f59a28
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-06-21 16:04:49 +0200

    make board size dynamic

--- game.js
+++ game.js
@@ -1,10 +1,23 @@
-initGame();
+
+
+
+const BOARD_WIDTH = 20;
+const BOARD_HEIGHT = 20;
+const CELL_SIZE = 30;
+let player_pos = Math.floor(BOARD_WIDTH / 2);
+
+
+
 
 function initGame() {
     createBoardDivs();
 }
+
 function createBoardDivs() {
+
     const grid = document.querySelector('.grid');
+    grid.style.width = `${CELL_SIZE * BOARD_WIDTH}px`;
+    grid.style.height = `${CELL_SIZE * BOARD_HEIGHT}px`;
     let row = -1;
     for (let i = 0; i < 400; i++) {
         if (i % 20 === 0) {
@@ -12,7 +25,17 @@ function createBoardDivs() {
         }
         grid.insertAdjacentHTML(
             'beforeend',
-            `<div class="square" data-row="${row}" data-col="${(i%20)}"></div>`
+            `<div class="square" data-row="${row}" data-col="${(i % 20)}"></div>`
         );
     }
+    const cells = document.querySelectorAll(".grid div");
+
+    cells.forEach((cell) => {
+        cell.style.width = `${CELL_SIZE}px`;
+        cell.style.height = `${CELL_SIZE}px`;
+    });
 }
+
+
+
+initGame();
